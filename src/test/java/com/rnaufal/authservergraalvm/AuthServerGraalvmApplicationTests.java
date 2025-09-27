d4bceaf61fd9c433dd45ed2720ecb4c96b0ad16a
Rafael Naufal <rafael.naufal@gmail.com>
2025-09-27 18:29:28 -0300

    Add OAuth2 authorization server configuration and integration tests

--- src/test/java/com/rnaufal/authservergraalvm/AuthServerGraalvmApplicationTests.java
+++ src/test/java/com/rnaufal/authservergraalvm/AuthServerGraalvmApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AuthServerGraalvmApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
